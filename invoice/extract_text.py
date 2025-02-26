c8f0d2e738350262c1cd1c1934f262b3c6a8f0c0
ricgir <148371705+ricgir@users.noreply.github.com>
2025-02-26 08:28:05 +0530

    Added extract text from invoice image function

--- invoice/extract_text.py
+++ invoice/extract_text.py
@@ -1,3 +1,6 @@
+import requests
+import base64
+import re
 from PIL import Image
 import pytesseract
 import argparse
@@ -5,17 +8,9 @@ import cv2
 import os
 import imutils
 
-
-# img = Image.open('receipt_2.jpeg')
-
-# text = pytesseract.image_to_string(img)
-
-# print(text)
 def extract_text_from_image(image_path):
     """Extract text directly using Tesseract OCR without preprocessing."""
     custom_config = r'--oem 3 --psm 6'  # OCR Engine Mode 3, PSM 6 (Assumes a block of text)
     img = Image.open(image_path)
     text = pytesseract.image_to_string(img, config=custom_config)
     return text
-
-print(extract_text_from_image("./invoice_4.png"))
